fba0b0dc3de886407dd8e4dc317bc7da5f53a645
Sudipto Saha <sahasudipto336@gmail.com>
2024-01-19 07:44:48 +0530

    added promise apis

---
+++ promise/promise_api/promise_any_failure.js
@@ -0,0 +1,18 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => reject("P1 fail"), 1000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject("P2 fail"), 2000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => reject("P3 fail"), 3000);
+});
+
+Promise.any([p1, p2, p3])
+  .then((res) => console.log(res))
+  .catch((err) => {
+    console.error(err);
+    console.log(err.errors);
+  });
